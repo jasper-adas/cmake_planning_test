453e9e354b0896da9e455a4b8f171fb048d91154
ZengHui <zenghuixm1212@163.com>
2024-04-11 21:07:54 +0800

    version 0.0.3

--- src/process/process.cpp
+++ src/process/process.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 
 #include "process.h"
+#include <Eigen/Dense>
 
 using namespace std;
+using Eigen::MatrixXd;
 
 void Process::planProcess()
 {
     cout << "this is planning process!" << endl;
     my_map.mapInfo();
+
+    MatrixXd m(2, 2);
+    m(0, 0) = 3;
+    m(1, 0) = 3;
+    m(0, 1) = 3;
+    m(1, 1) = 3;
+    cout << m << endl;
+
     cout << "planning sucess!" << endl;
 }
